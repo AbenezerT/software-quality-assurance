552c48c0b59076fa83d98a7e5324be020eb6d676
Abenezer <Abenezertesma@gmail.com>
2020-01-30 14:24:42 -0500

    containing the completed Circle class

---
+++ AbeCircle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbeAssignment1
+{
+    public class AbeCircle
+    {
+        private int radius;
+
+        //Default Constructor which sets the value of the radius to 1
+
+        public AbeCircle()
+        {
+            this.radius = 1;
+        }
+        //Non-default Constructor which sets the value of the radius to the value passed
+        public AbeCircle(int radius)
+        {
+            this.radius = radius;
+        }
+        //Returns the value of the Circle radius
+        public int GetRadius()
+        {
+            return radius;
+
+        }
+        //Sets the value of the Circle radius
+        public void SetRadius(int radius)
+        {
+            this.radius = radius;
+        }
+
+        //Calculates and returns the circumference of the Circle
+        public double GetCircumference()
+        {
+            return 2 * Math.PI * radius;
+        }
+        // Calculates and returns the area of the Circle
+        public double GetArea()
+        {
+            return Math.PI * radius * radius;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,78 @@ namespace AbeAssignment1
     {
         static void Main(string[] args)
         {
+            int radius;
+            Console.Write("Please enter the radius of the circle in number: ");
+
+
+            while (!int.TryParse(Console.ReadLine(), out radius) || radius <= 0)
+            {
+                Console.Clear();
+                Console.WriteLine("You entered an invalid input");
+                Console.Write("Please enter the radius of the circle in number: ");
+            }
+
+            AbeCircle circle = new AbeCircle(radius);
+            int option;
+
+
+            do
+            //while option is not equale to = 5
+            {
+
+                Menu();
+                //validating menu inputs
+                while (!int.TryParse(Console.ReadLine(), out option))
+                {
+                    Console.Clear();
+                    Console.WriteLine("You entered an invalid input");
+                    Menu();
+                }
+                if (option == 1) //diplay radius
+                {
+                    Console.WriteLine("The radius of the circle is: " + circle.GetRadius());
+                }
+                else if (option == 2)//change radius
+                {
+                     // !!!!!!!!!!! 
+                   //Console.Write("Enter the new radius of the circle: ");
+                    radius = Int32.Parse(Console.ReadLine());
+                    while ( radius <= 0 )  
+                    {
+                        Console.WriteLine("The radius must be an integer greater than zero");
+                        Console.Write("Please enter again: ");
+                        radius = Int32.Parse(Console.ReadLine());
+                    }
+                    circle.SetRadius(radius);
+                }
+                else if (option == 3) //display circumference
+                {
+                    Console.WriteLine("The circumference of the circle is: " + circle.GetCircumference());
+                }
+                else if (option == 4)//display area
+                {
+                    Console.WriteLine("The area of the circle is: " + circle.GetArea());
+                }
+                else if (option == 5)//exit
+                {
+                    Console.WriteLine("Goodbye");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid option!");
+                }
+            } while (option != 5);
+            Console.ReadLine();
+
+        }
+        //Displays menu
+        static void Menu()
+        {
+            Console.WriteLine("1.Get Circle Radius");
+            Console.WriteLine("2.Change Circle Radius");
+            Console.WriteLine("3.Get Circle Circumference");
+            Console.WriteLine("4.Get Circle Area");
+            Console.WriteLine("5.Exit");
         }
     }
 }
